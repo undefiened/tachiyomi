12f5ebbfcf0b2fdac2848694188256de92cdfc2e
KaiserBh <kaiserbh@proton.me>
2023-04-10 01:59:27 +1000

    feat: init dependency.

--- app/src/main/java/eu/kanade/tachiyomi/AppModule.kt
+++ app/src/main/java/eu/kanade/tachiyomi/AppModule.kt
@@ -49,6 +49,7 @@ import tachiyomi.domain.backup.service.BackupPreferences
 import tachiyomi.domain.download.service.DownloadPreferences
 import tachiyomi.domain.library.service.LibraryPreferences
 import tachiyomi.domain.source.service.SourceManager
+import tachiyomi.domain.sync.SyncPreferences
 import tachiyomi.source.local.image.LocalCoverManager
 import tachiyomi.source.local.io.LocalSourceFileSystem
 import uy.kohesive.injekt.api.InjektModule
@@ -195,6 +196,9 @@ class PreferenceModule(val application: Application) : InjektModule {
                 folderProvider = get<AndroidBackupFolderProvider>(),
                 preferenceStore = get(),
             )
+        }
+        addSingletonFactory {
+            SyncPreferences(get())
         }
         addSingletonFactory {
             UiPreferences(get())

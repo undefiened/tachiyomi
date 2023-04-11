69a706be8cb0cfb559b7d9ef48d55cc52a465e79
KaiserBh <kaiserbh@proton.me>
2023-04-12 05:11:15 +1000

    chore: added last_sync, last_changes made on device sync interval, device name and id.

--- domain/src/main/java/tachiyomi/domain/sync/SyncPreferences.kt
+++ domain/src/main/java/tachiyomi/domain/sync/SyncPreferences.kt
@@ -1,11 +1,19 @@
 package tachiyomi.domain.sync
 
 import tachiyomi.core.preference.PreferenceStore
+import java.time.Instant
 
 class SyncPreferences(
     private val preferenceStore: PreferenceStore,
 ) {
-
     fun syncHost() = preferenceStore.getString("sync_host", "https://sync.tachiyomi.org")
     fun syncAPIKey() = preferenceStore.getString("sync_api_key", "")
+    fun syncLastLocalUpdate() = preferenceStore.getInstant("sync_last_local_update", Instant.EPOCH)
+    fun syncLastSync() = preferenceStore.getInstant("sync_last_sync", Instant.EPOCH)
+
+    fun syncInterval() = preferenceStore.getInt("sync_interval", 0)
+
+    fun deviceName() = preferenceStore.getString("device_name", android.os.Build.MANUFACTURER + android.os.Build.PRODUCT)
+
+    fun deviceID() = preferenceStore.getInt("device_id", 0)
 }

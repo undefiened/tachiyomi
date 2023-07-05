e267c1d7807ed87257ff26ec40c8e49bc1b834f0
Aria Moradi <aria.moradi007@gmail.com>
2023-07-05 23:09:24 +0330

    refactor round 1

--- app/src/main/java/eu/kanade/tachiyomi/data/sync/SyncManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/sync/SyncManager.kt
@@ -11,16 +11,10 @@ import eu.kanade.tachiyomi.data.backup.models.BackupManga
 import eu.kanade.tachiyomi.data.sync.models.SData
 import eu.kanade.tachiyomi.data.sync.models.SyncDevice
 import eu.kanade.tachiyomi.data.sync.models.SyncStatus
-import eu.kanade.tachiyomi.network.POST
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
+import eu.kanade.tachiyomi.data.sync.service.GoogleDriveSyncService
+import eu.kanade.tachiyomi.data.sync.service.SyncYomiSyncService
 import kotlinx.serialization.json.Json
 import logcat.LogPriority
-import okhttp3.Headers
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.OkHttpClient
-import okhttp3.RequestBody.Companion.gzip
-import okhttp3.RequestBody.Companion.toRequestBody
 import tachiyomi.core.util.system.logcat
 import tachiyomi.data.Chapters
 import tachiyomi.data.DatabaseHandler
@@ -55,9 +49,7 @@ class SyncManager(
     private val notifier: SyncNotifier = SyncNotifier(context)
 
     enum class SyncService(val value: Int) {
-        NONE(0),
-        GOOGLE_DRIVE(1),
-        SELF_HOSTED(2),
+        NONE(0), GOOGLE_DRIVE(1), SELF_HOSTED(2),
         ;
 
         companion object {
@@ -66,13 +58,10 @@ class SyncManager(
     }
 
     /**
-     * Syncs data with the remote server or Google Drive.
+     * Syncs data with a sync service.
      *
      * This function retrieves local data (favorites, manga, extensions, and categories)
-     * from the database using the BackupManager, then synchronizes the data with the remote
-     * server or uploads the data to Google Drive.
-     * The server URL, API key, or Google Drive are determined based on the sync preferences.
-     * The data is sent to the server or uploaded to Google Drive by calling `sendSyncData()`.
+     * from the database using the BackupManager, then synchronizes the data with a sync service.
      */
     suspend fun syncData() {
         val databaseManga = getAllMangaFromDB()
@@ -85,53 +74,6 @@ class SyncManager(
             backupManager.backupExtensionInfo(databaseManga),
         )
 
-        when (val syncService = SyncService.fromInt(syncPreferences.syncService().get())) {
-            SyncService.GOOGLE_DRIVE -> {
-                syncDataWithServer(
-                    SyncService.GOOGLE_DRIVE,
-                    null,
-                    null,
-                    backup,
-                )
-            }
-
-            SyncService.SELF_HOSTED -> {
-                val host = syncPreferences.syncHost().get()
-                val apiKey = syncPreferences.syncAPIKey().get()
-                val url = "$host/api/sync/data"
-
-                syncDataWithServer(
-                    SyncService.SELF_HOSTED,
-                    url,
-                    apiKey,
-                    backup,
-                )
-            }
-
-            else -> {
-                logcat(LogPriority.ERROR) { "Invalid sync service type: $syncService" }
-            }
-        }
-    }
-
-    /**
-     * Sends a sync request to the server containing backup data.
-     *
-     * This function constructs the required objects (SyncStatus, SyncDevice, and SData)
-     * using the given input parameters, converts the SData object to a JSON string,
-     * and sends the JSON data to the server or uploads it to Google Drive depending on the sync service.
-     *
-     * @param syncService The sync service type (Google Drive or Self-hosted).
-     * @param url The server URL to send the sync request to (only required for Self-hosted sync).
-     * @param apiKey The API key for authentication (only required for Self-hosted sync).
-     * @param backup A Backup object containing the manga, categories, and extensions data to be synced.
-     */
-    private suspend fun syncDataWithServer(
-        syncService: SyncService,
-        url: String?,
-        apiKey: String?,
-        backup: Backup,
-    ) {
         // Create the SyncStatus object
         val syncStatus = SyncStatus(
             lastSynced = Instant.now().toString(),
@@ -151,143 +93,36 @@ class SyncManager(
             device = device,
         )
 
-        // Convert the SyncData object to a JSON string
-        val jsonData = json.encodeToString(syncData)
-
         // Handle sync based on the selected service
-        when (syncService) {
+        val syncService = when (val syncService = SyncService.fromInt(syncPreferences.syncService().get())) {
             SyncService.GOOGLE_DRIVE -> {
-                sendSyncData(
-                    url = "",
-                    apiKey = "",
-                    jsonData = jsonData,
-                    storageType = "GoogleDrive",
-                )
+                GoogleDriveSyncService(context, json, syncPreferences)
             }
 
             SyncService.SELF_HOSTED -> {
-                if (url != null && apiKey != null) {
-                    sendSyncData(url, apiKey, jsonData)
-                }
+                SyncYomiSyncService(
+                    context,
+                    json,
+                    syncPreferences,
+                    notifier,
+                )
             }
 
             else -> {
                 logcat(LogPriority.ERROR) { "Invalid sync service type: $syncService" }
+                null
             }
         }
-    }
 
-    /**
-     * Sends the sync data to the server as a compressed Gzip or uploads it to Google Drive.
-     *
-     * This function prepares an HTTP POST request with the given URL, API key, and JSON data for the server
-     * or uploads the JSON data to Google Drive depending on the storageType.
-     * The JSON data is sent in the request body, and the API key is added as a header for server requests.
-     * After sending the data, the function updates the local data if required.
-     *
-     * If the storageType is "GoogleDrive", the function uploads the JSON data to Google Drive using the GoogleDriveSync class.
-     * If the upload is successful, the function decodes the combined JSON data and updates the local data with the new backup.
-     * The sync last sync preference is then updated with the current time.
-     *
-     * If the storageType is null (indicating a server sync), the function sends an HTTP POST request to the server with the given URL and API key.
-     * The JSON data is sent as a compressed Gzip in the request body, and the API key is added as a header.
-     * If the request is successful, the function decodes the sync data, updates the local data with the new backup, and updates the sync last sync preference with the current time.
-     * If the device ID is 0 or different from the server device ID, the function also updates the local device ID preference.
-     *
-     * @param url The server URL to send the sync data to.
-     * @param apiKey The API key for authentication.
-     * @param jsonData The JSON string containing the sync data.
-     * @param storageType The storage type to use for sync (e.g., "GoogleDrive"). Defaults to null for server sync.
-     */
-    private suspend fun sendSyncData(
-        url: String,
-        apiKey: String,
-        jsonData: String,
-        storageType: String? = null,
-    ) {
-        if (storageType == "GoogleDrive") {
-            logcat(
-                LogPriority.DEBUG,
-            ) { "GoogleDrive sync started!" }
-            val googleDriveSync = GoogleDriveSync(context)
-            googleDriveSync.refreshToken()
-            val combinedJsonData = googleDriveSync.uploadToGoogleDrive(jsonData)
-            if (combinedJsonData != null) {
-                val backup = decodeSyncBackup(combinedJsonData)
-                val (filteredFavorites, nonFavorites) = filterFavoritesAndNonFavorites(backup)
-                updateNonFavorites(nonFavorites)
-                SyncHolder.backup = backup.copy(backupManga = filteredFavorites)
-                BackupRestoreJob.start(context, "".toUri(), true)
-                syncPreferences.syncLastSync().set(Instant.now())
-                logcat(
-                    LogPriority.DEBUG,
-                ) { "GoogleDrive sync completed!" }
-                return
-            }
-            logcat(
-                LogPriority.DEBUG,
-            ) { "GoogleDrive sync failed!" }
-        } else {
-            logcat(
-                LogPriority.DEBUG,
-            ) { "SyncYomi sync started!" }
+        val remoteBackup = syncService?.doSync(syncData)
 
-            val client = OkHttpClient()
-            val mediaType = "application/gzip".toMediaTypeOrNull()
-            val body = jsonData.toRequestBody(mediaType).gzip()
-
-            val headers = Headers.Builder()
-                .add("Content-Type", "application/gzip")
-                .add("Content-Encoding", "gzip")
-                .add("X-API-Token", apiKey)
-                .build()
-
-            val request = POST(
-                url = url,
-                headers = headers,
-                body = body,
-            )
-
-            client.newCall(request).execute().use { response ->
-                val responseBody = response.body.string()
-
-                if (response.isSuccessful) {
-                    val syncDataResponse: SData = json.decodeFromString(responseBody)
-
-                    val backup = decodeSyncBackup(responseBody)
-                    val (filteredFavorites, nonFavorites) = filterFavoritesAndNonFavorites(backup)
-                    updateNonFavorites(nonFavorites)
-                    SyncHolder.backup = backup.copy(backupManga = filteredFavorites)
-                    BackupRestoreJob.start(context, "".toUri(), true)
-                    syncPreferences.syncLastSync().set(Instant.now())
-
-                    // If the device ID is 0 and not equal to the server device ID (this happens when the DB is fresh and the app is not), update it
-                    if (syncPreferences.deviceID().get() == 0 || syncPreferences.deviceID()
-                        .get() != syncDataResponse.device?.id
-                    ) {
-                        syncDataResponse.device?.id?.let { syncPreferences.deviceID().set(it) }
-                    }
-
-                    logcat(
-                        LogPriority.DEBUG,
-                    ) { "SyncYomi sync completed!" }
-                } else {
-                    notifier.showSyncError("Failed to sync: $responseBody")
-                    responseBody.let { logcat(LogPriority.ERROR) { "SyncError:$it" } }
-                }
-            }
+        if (remoteBackup != null) {
+            val (filteredFavorites, nonFavorites) = filterFavoritesAndNonFavorites(remoteBackup)
+            updateNonFavorites(nonFavorites)
+            SyncHolder.backup = backup.copy(backupManga = filteredFavorites)
+            BackupRestoreJob.start(context, "".toUri(), true)
+            syncPreferences.syncLastSync().set(Instant.now())
         }
-    }
-
-    /**
-     * Decodes the given sync data string into a Backup object.
-     *
-     * @param data The sync data string to be decoded.
-     * @return The decoded Backup object.
-     */
-    private fun decodeSyncBackup(data: String): Backup {
-        val sData = json.decodeFromString(SData.serializer(), data)
-        return sData.backup!!
     }
 
     /**
@@ -309,22 +144,7 @@ class SyncManager(
         val localChapters = handler.await { chaptersQueries.getChaptersByMangaId(localManga.id).executeAsList() }
         val localCategories = getCategories.await(localManga.id).map { it.order }
 
-        return localManga.source != remoteManga.source ||
-            localManga.url != remoteManga.url ||
-            localManga.title != remoteManga.title ||
-            localManga.artist != remoteManga.artist ||
-            localManga.author != remoteManga.author ||
-            localManga.description != remoteManga.description ||
-            localManga.genre != remoteManga.genre ||
-            localManga.status.toInt() != remoteManga.status ||
-            localManga.thumbnailUrl != remoteManga.thumbnailUrl ||
-            localManga.dateAdded != remoteManga.dateAdded ||
-            localManga.chapterFlags.toInt() != remoteManga.chapterFlags ||
-            localManga.favorite != remoteManga.favorite ||
-            localManga.viewerFlags.toInt() != remoteManga.viewer_flags ||
-            localManga.updateStrategy != remoteManga.updateStrategy ||
-            areChaptersDifferent(localChapters, remoteManga.chapters) ||
-            localCategories != remoteManga.categories
+        return localManga.source != remoteManga.source || localManga.url != remoteManga.url || localManga.title != remoteManga.title || localManga.artist != remoteManga.artist || localManga.author != remoteManga.author || localManga.description != remoteManga.description || localManga.genre != remoteManga.genre || localManga.status.toInt() != remoteManga.status || localManga.thumbnailUrl != remoteManga.thumbnailUrl || localManga.dateAdded != remoteManga.dateAdded || localManga.chapterFlags.toInt() != remoteManga.chapterFlags || localManga.favorite != remoteManga.favorite || localManga.viewerFlags.toInt() != remoteManga.viewer_flags || localManga.updateStrategy != remoteManga.updateStrategy || areChaptersDifferent(localChapters, remoteManga.chapters) || localCategories != remoteManga.categories
     }
 
     /**
@@ -342,15 +162,7 @@ class SyncManager(
 
         return remoteChapters.any { remoteChapter ->
             localChapterMap[remoteChapter.url]?.let { localChapter ->
-                localChapter.name != remoteChapter.name ||
-                    localChapter.scanlator != remoteChapter.scanlator ||
-                    localChapter.read != remoteChapter.read ||
-                    localChapter.bookmark != remoteChapter.bookmark ||
-                    localChapter.last_page_read != remoteChapter.lastPageRead ||
-                    localChapter.date_fetch != remoteChapter.dateFetch ||
-                    localChapter.date_upload != remoteChapter.dateUpload ||
-                    localChapter.chapter_number != remoteChapter.chapterNumber ||
-                    localChapter.source_order != remoteChapter.sourceOrder
+                localChapter.name != remoteChapter.name || localChapter.scanlator != remoteChapter.scanlator || localChapter.read != remoteChapter.read || localChapter.bookmark != remoteChapter.bookmark || localChapter.last_page_read != remoteChapter.lastPageRead || localChapter.date_fetch != remoteChapter.dateFetch || localChapter.date_upload != remoteChapter.dateUpload || localChapter.chapter_number != remoteChapter.chapterNumber || localChapter.source_order != remoteChapter.sourceOrder
             } ?: true
         }
     }
--- app/src/main/java/eu/kanade/tachiyomi/ui/setting/track/GoogleDriveLoginActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/setting/track/GoogleDriveLoginActivity.kt
@@ -3,13 +3,13 @@ package eu.kanade.tachiyomi.ui.setting.track
 import android.net.Uri
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
-import eu.kanade.tachiyomi.data.sync.GoogleDriveSync
+import eu.kanade.tachiyomi.data.sync.service.GoogleDriveSyncService
 import tachiyomi.core.util.lang.launchIO
 import uy.kohesive.injekt.Injekt
 import uy.kohesive.injekt.api.get
 
 class GoogleDriveLoginActivity : BaseOAuthLoginActivity() {
-    private val googleDriveSync = Injekt.get<GoogleDriveSync>()
+    private val googleDriveSync = Injekt.get<GoogleDriveSyncService>()
     override fun handleResult(data: Uri?) {
         val code = data?.getQueryParameter("code")
         val error = data?.getQueryParameter("error")
